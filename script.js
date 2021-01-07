b429c601c62785a3da3a3eeb39d634e924e76936
TGentr <50638963+TGentr@users.noreply.github.com>
2021-01-07 19:59:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,16 @@
 const quotes = [
 {
-    name: 'Yoda',
-    quote: 'Always pass on what you have learned',
+    name: 'restauracja 1',
+    quote: 'adres',
 },
 {
-    name: 'Yoda',
-    quote: 'Train yourself to let go of everything you fear to lose.',
+    name: 'restauracja 2',
+    quote: 'adres 2',
 },
 {
-    name: 'Darth Vader',
-    quote: 'The circle is now complete. When I left you, I was but the learner. Now I am the master.',
+    name: 'restauracja 3',
+    quote: 'adres 3',
 },
-{
-    name: 'Leia',
-    quote: 'Help me, Obi-Wan Kenobi. You’re my only hope.',
-},
-{
-    name: 'Emperor Palpatine',
-    quote: 'There is a great disturbance in the Force.',
-},
-{
-    name: 'Yoda',
-    quote: 'Everything is proceeding as I have foreseen.',
-},
-{
-    name: 'Luke Skywalker',
-    quote: 'There IS still good in him. I’ve felt it.',
-},
-{
-    name: 'Luke Skywalker',
-    quote: 'If I don’t make it back, you’re the only hope for the Alliance.',
-},
-{
-    name: 'General Ackbar',
-    quote: 'It\'s a trap!',
-
-},
-{
-    name: 'Han Solo',
-    quote: 'Don\'t everybody thank me at once',
-},
-{
-    name: 'Han Solo',
-    quote: 'You know, sometimes I amaze even myself.'
-}
 ]
 
 //zmienne
