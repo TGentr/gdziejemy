1fb66aa4524992a8446a3f55e616e1f82d7029e7
p-w-g <p.grzelczyk@gmail.com>
2021-09-13 21:47:42 +0200

    removes redundancies

--- script.js
+++ script.js
@@ -1,44 +1,30 @@
 const quotes = [
-    {
-        name: 'Wielka 23/2',
-        quote: 'Vegab',
-    },
-    {
-        name: 'Piekary 17',
-        quote: 'miXtura',
-    },
-    {
-        name: 'Limanowskiego 2',
-        quote: 'Vege Pizza',
-    },
-    {
-        name: '23 Lutego 29/33',
-        quote: 'Porażka',
-    },
-]
+  {
+    name: 'Wielka 23/2',
+    quote: 'Vegab',
+  },
+  {
+    name: 'Piekary 17',
+    quote: 'miXtura',
+  },
+  {
+    name: 'Limanowskiego 2',
+    quote: 'Vege Pizza',
+  },
+  {
+    name: '23 Lutego 29/33',
+    quote: 'Porażka',
+  },
+];
 
-//zmienne
+const buttonn = document.querySelector('#wybierz');
+const quotee = document.querySelector('#quote');
+const autor = document.querySelector('#autor');
+buttonn.addEventListener('click', showRandomEatery);
 
-const buttonn = document.querySelector("#wybierz");
-const quotee = document.querySelector("#quote");
-const autor = document.querySelector("#autor");
-buttonn.addEventListener('click', pokazuj);
+const showRandomEatery = () => {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
 
-function pokazuj() {
-
-    // random number from array
-
-    let rande = Math.floor(Math.random() * quotes.length);
-    console.log(rande);
-    //problem: losowe liczby nie mogą się powtarzać do zrobienia next
-
-    //quotee.innerHTML = `${quotes[1].quote}`;
-    //quotee.innerHTML = `${quotes[rande].quote}`;
-
-
-    quotee.innerHTML = quotes[rande].quote;
-    autor.innerHTML = quotes[rande].name;
-
-
-}
-
+  quotee.innerHTML = quotes[randomNumber].quote;
+  autor.innerHTML = quotes[randomNumber].name;
+};
