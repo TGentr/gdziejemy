dc07f34b37b5492c750c9d42a761cd4dbcd48c08
p-w-g <p.grzelczyk@gmail.com>
2021-09-13 21:43:35 +0200

    lint, remove whitespace

--- script.js
+++ script.js
@@ -15,30 +15,30 @@ const quotes = [
         name: '23 Lutego 29/33',
         quote: 'Porażka',
     },
-    ]
-    
-    //zmienne
-    
-    const buttonn = document.querySelector("#wybierz");
-    const quotee = document.querySelector("#quote");
-    const autor = document.querySelector("#autor");
-    buttonn.addEventListener('click', pokazuj);
-    
-    function pokazuj (){
-    
+]
+
+//zmienne
+
+const buttonn = document.querySelector("#wybierz");
+const quotee = document.querySelector("#quote");
+const autor = document.querySelector("#autor");
+buttonn.addEventListener('click', pokazuj);
+
+function pokazuj() {
+
     // random number from array
-    
-    let rande = Math.floor(Math.random()*quotes.length);
+
+    let rande = Math.floor(Math.random() * quotes.length);
     console.log(rande);
     //problem: losowe liczby nie mogą się powtarzać do zrobienia next
-    
+
     //quotee.innerHTML = `${quotes[1].quote}`;
     //quotee.innerHTML = `${quotes[rande].quote}`;
-    
-    
+
+
     quotee.innerHTML = quotes[rande].quote;
     autor.innerHTML = quotes[rande].name;
-    
-    
-    }
-    
+
+
+}
+
