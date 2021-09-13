b7462c536d065c0a58c078f65863d2675a4e5273
TGentr <50638963+TGentr@users.noreply.github.com>
2021-09-13 21:03:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
 const quotes = [
-{
-    name: 'restauracja 1',
-    quote: 'adres',
-},
-{
-    name: 'restauracja 2',
-    quote: 'adres 2',
-},
-{
-    name: 'restauracja 3',
-    quote: 'adres 3',
-},
-]
-
-//zmienne
-
-const buttonn = document.querySelector("#wybierz");
-const quotee = document.querySelector("#quote");
-const autor = document.querySelector("#autor");
-buttonn.addEventListener('click', pokazuj);
-
-function pokazuj (){
-
-// random number from array
-
-let rande = Math.floor(Math.random()*quotes.length);
-console.log(rande);
-//problem: losowe liczby nie mogą się powtarzać do zrobienia next
-
-//quotee.innerHTML = `${quotes[1].quote}`;
-//quotee.innerHTML = `${quotes[rande].quote}`;
-
-
-quotee.innerHTML = quotes[rande].quote;
-autor.innerHTML = quotes[rande].name;
-
-
-}
-
-
+    {
+        name: 'Wielka 23/2',
+        quote: 'Vegab',
+    },
+    {
+        name: 'Piekary 17',
+        quote: 'miXtura',
+    },
+    {
+        name: 'Limanowskiego 2',
+        quote: 'Vege Pizza',
+    },
+    {
+        name: '23 Lutego 29/33',
+        quote: 'Porażka',
+    },
+    ]
+    
+    //zmienne
+    
+    const buttonn = document.querySelector("#wybierz");
+    const quotee = document.querySelector("#quote");
+    const autor = document.querySelector("#autor");
+    buttonn.addEventListener('click', pokazuj);
+    
+    function pokazuj (){
+    
+    // random number from array
+    
+    let rande = Math.floor(Math.random()*quotes.length);
+    console.log(rande);
+    //problem: losowe liczby nie mogą się powtarzać do zrobienia next
+    
+    //quotee.innerHTML = `${quotes[1].quote}`;
+    //quotee.innerHTML = `${quotes[rande].quote}`;
+    
+    
+    quotee.innerHTML = quotes[rande].quote;
+    autor.innerHTML = quotes[rande].name;
+    
+    
+    }
+    
